dbc180379391207828c0d9a2c8ddbe6f3a7d4736
leoycyang@gmail.com <junyang@gmail.com>
2025-03-28 17:11:35 -0400

    add SQL example

--- web/test.py
+++ web/test.py
@@ -7,3 +7,15 @@ bp = Blueprint('test', __name__)
 @bp.route('/')
 def hello():
     return 'Hello, World!'
+
+@bp.route('/test')
+def test():
+    lines = list()
+    for rowid, _, _, full_text in app.db.execute('''
+SELECT rowid, case_id, case_title, full_text
+FROM wenshu
+WHERE full_text REGEXP :pattern
+LIMIT :limit OFFSET 0
+''', pattern='指导.{0,10}案例', limit=5):
+        lines.append(f'{rowid}: {full_text}')
+    return '\n'.join(lines)
